d19b8a15460d54616db7277f7f15e5ac95806eb4
arifremal <arifremal013@gmail.com>
2023-01-28 11:10:54 +0600

    file created

--- revision.js
+++ revision.js
@@ -1,2 +1,20 @@
 var bottle = 'water';
-var color = yellow
+var bottleColor = 'yellow';
+var Sunglassprice =900;
+
+var tableMatrials =  ['watch', 'stand ', 'mouse', 'paper' ];
+var item = tableMatrials[2];
+tableMatrials[4] = 'bago';
+var mouse = tableMatrials.indexOf('mouse');
+if (tableMatrials.length> 4){
+    console.log('it is');
+}
+else if( tableMatrials[2] == 'pen' ){
+    console.log('its pen');
+
+}
+else{
+    console.log('Nothing here');
+}
+
+
